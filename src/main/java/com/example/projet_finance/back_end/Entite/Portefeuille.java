37dff0e018f05f768ec73a516f4fd34ec456fafd
un_ours_blanc <46861286+loursblancgithub@users.noreply.github.com>
2024-01-12 15:49:45 +0100

    Revert "autresmodifsStructure"
    This reverts commit 4117641492b14cc4085761077fe07addc421a6bc.

--- src/main/java/com/example/projet_finance/back_end/Entite/Portefeuille.java
+++ src/main/java/com/example/projet_finance/back_end/Entite/Portefeuille.java
@@ -1,5 +1,8 @@
 package com.example.projet_finance.back_end.Entite;
 
+import com.example.projet_finance.back_end.Action;
+import com.example.projet_finance.back_end.Banque.CompteCourant;
+
 abstract class Portefeuille {
 
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,12 +5,10 @@ module com.example.projet_finance {
     requires org.json;
 
 
-    opens front_end to javafx.fxml;
-    exports front_end;
-    opens front_end.Accueil to javafx.fxml;
-    exports front_end.Accueil to javafx.fxml;
-    exports front_end.Virement;
-    opens front_end.Virement to javafx.fxml;
-    exports front_end.Authentification;
-    opens front_end.Authentification to javafx.fxml;
+    opens front_end_Authentification to javafx.fxml;
+    exports front_end_Authentification;
+    opens front_end_Authentification.Accueil to javafx.fxml;
+    exports front_end_Authentification.Accueil to javafx.fxml;
+    exports front_end_Authentification.Virement;
+    opens front_end_Authentification.Virement to javafx.fxml;
 }
