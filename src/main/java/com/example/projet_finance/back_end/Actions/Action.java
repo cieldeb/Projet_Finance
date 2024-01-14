add06556ca86e38da5b3df13bf2e324fd9f43d64
Gabgab58 <gmosselmans03@gmail.com>
2024-01-14 23:49:12 +0100

    ObjetDeBase
    Ajout des objets de base pour le back end.

--- src/main/java/com/example/projet_finance/back_end/Actions/Action.java
+++ src/main/java/com/example/projet_finance/back_end/Actions/Action.java
@@ -1,4 +1,19 @@
 package com.example.projet_finance.back_end.Actions;
 
 public class Action {
+    protected String name;
+    protected String symbol;
+    protected float value;
+    protected int quantite;
+    protected float valeurTotale;
+
+
+    public Action(String name,String symbol,float value,int quantite,float valeurTotale) {
+        this.name = name;
+        this.symbol = symbol;
+        this.value = value;
+        this.quantite = quantite;
+        this.valeurTotale = valeurTotale;
+    }
 }
+
--- src/main/java/com/example/projet_finance/back_end/Entite/Portefeuille.java
+++ src/main/java/com/example/projet_finance/back_end/Entite/Portefeuille.java
@@ -1,5 +1,22 @@
 package com.example.projet_finance.back_end.Entite;
 
-abstract class Portefeuille {
+import com.example.projet_finance.back_end.Actions.Action;
+import com.example.projet_finance.back_end.Banque.Compte;
+import com.example.projet_finance.back_end.Crypto.Crypto;
 
+import java.util.LinkedList;
+
+class Portefeuille {
+    protected String name;
+    protected LinkedList<Compte> listeComptes;
+    protected LinkedList<Action> listActions;
+    protected LinkedList<Crypto> listCrypto;
+
+    //Construteur:
+    public Portefeuille(String name, LinkedList<Compte> listeComptes, LinkedList<Action> listActions, LinkedList<Crypto> listCrypto) {
+        this.name = name;
+        this.listeComptes = listeComptes;
+        this.listActions = listActions;
+        this.listCrypto = listCrypto;
+    }
 }
